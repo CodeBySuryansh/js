95ca84b7432ba448c43dc2e53cf399adacf80625
Suryansh Kumar Singh <suryansh0902ss@gmail.com>
2025-05-29 07:42:50 +0000

    changes made

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,11 @@
+// alert(2+2) we are using node js,not browser
+console.log(3+3)
+console.log("ramesh")
+
+
+let name ="hitesh" //string
+let age=18 //number
+let isloggedIn= false //boolean true or false
+//null==standalone value
+//undefined
+console.log(typeof name)
---
+++ 01_basics/03_dataConversion.js
@@ -0,0 +1,22 @@
+let score="33"
+let score2="123abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let ValueInNumber=Number(score);
+let ValueInNumber2=Number(score2);
+
+console.log(typeof(ValueInNumber));
+console.log(typeof(ValueInNumber2)); //here "123abc" will converted from string to number
+
+console.log(ValueInNumber2)//will give NaN not a number
+
+//------------------------operations--------------------
+let value =2
+let negValue =-value
+console.log(negValue);
+
+
+
+
