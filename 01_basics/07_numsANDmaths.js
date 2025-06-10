99434ba1b6813e3b261fe2a265bda0e4ca08b795
Suryansh Kumar Singh <suryansh0902ss@gmail.com>
2025-06-10 12:13:45 +0000

    changes made

---
+++ 01_basics/07_numsANDmaths.js
@@ -0,0 +1,28 @@
+const score = 400 
+const balance =new Number(100)//object of number type it has number methods and properperties
+// console.log(balance);
+// console.log(balance.toString().length)//converts number to string and find len
+// console.log(balance.toFixed(2))//add .00
+
+// const otherNumber=123.8966
+
+// console.log(otherNumber.toPrecision(4))
+// const hundred=100000
+// console.log(hundred.toLocaleString('en-IN'))
+
+//-------------------------------MAths--------------------------------
+//console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6))
+console.log(Math.ceil(4.6))
+console.log(Math.floor(4.5))
+console.log(Math.max(4,6,10,12))
+console.log(Math.floor(Math.random()*10)+1);
+
+
+const min=10
+const max=20
+
+Math.random()
+
+
---
+++ 01_basics/08_dateAndTime.js
@@ -0,0 +1,11 @@
+let myDate=new Date()
+// console.log(myDate);
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleString())
+// console.log(myDate.toLocaleDateString())
+// console.log(typeof myDate)
+
+let myCreatedDate=new Date(2023,1,23)
+console.log(myCreatedDate.toLocaleString())
+my
